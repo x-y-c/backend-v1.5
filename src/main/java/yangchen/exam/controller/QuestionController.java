60128852df42d53b5039e69704b1b294b37b891a
Yangchen <2015011446@mail.bistu.edu.cn>
2019-04-16 22:20:55 +0800

    添加题目部分的controller接口 添加日志

--- src/main/java/yangchen/exam/controller/QuestionController.java
+++ src/main/java/yangchen/exam/controller/QuestionController.java
@@ -1,5 +1,7 @@
 package yangchen.exam.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +11,9 @@ import yangchen.exam.model.Category;
 import yangchen.exam.model.JsonResult;
 import yangchen.exam.service.base.QuestionService;
 import yangchen.exam.service.biz.TestInfoService;
+import yangchen.exam.util.IpUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -22,43 +26,51 @@ import java.util.List;
 @RequestMapping(value = "/exam/question", produces = MediaType.APPLICATION_JSON_VALUE)
 public class QuestionController {
 
-
+    private static Logger LOGGER = LoggerFactory.getLogger(QuestionController.class);
     @Autowired
     private QuestionService questionService;
 
     @Autowired
     private TestInfoService testInfoService;
 
-    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
     public JsonResult createQuestion(@RequestBody Question question) {
         Question questionResult = questionService.createQuestion(question);
+        LOGGER.info("create question, the ip = [{}]", IpUtil.getIpAddr(httpServletRequest));
         return JsonResult.succResult(questionResult);
     }
 
     @RequestMapping(value = "/category", method = RequestMethod.GET)
     public JsonResult findQuestionByCategory(@RequestParam Category category) {
         List<Question> questionByCategory = questionService.findQuestionByCategory(category);
+        LOGGER.info("find question by category, the ip = [{}]", IpUtil.getIpAddr(httpServletRequest));
         return JsonResult.succResult(questionByCategory);
     }
 
-    @RequestMapping(value = "/quesionId",method = RequestMethod.GET)
+    @RequestMapping(value = "/questionId", method = RequestMethod.GET)
     public JsonResult findQuestionById(@RequestParam Integer id) {
         Question questionById = questionService.findQuestionById(id);
+        LOGGER.info("find question by Id,the ip = [{}]", IpUtil.getIpAddr(httpServletRequest));
         return JsonResult.succResult(questionById);
     }
 
-    @RequestMapping(value = "/remove",method = RequestMethod.POST)
+    @RequestMapping(value = "/remove", method = RequestMethod.POST)
     public JsonResult DeleteQuestionById(@RequestBody Integer id) {
         Boolean aBoolean = questionService.deleteQuestion(id);
         if (aBoolean) {
+            LOGGER.info("delete question by id, the ip = [{}]", IpUtil.getIpAddr(httpServletRequest));
             return JsonResult.succResult(null);
         }
         return JsonResult.errorResult("fail", "删除失败", null);
     }
 
-    @RequestMapping(value = "/update",method = RequestMethod.POST)
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
     public JsonResult updateQuestion(@RequestBody Question question) {
         Question questionResult = questionService.updateQuestion(question);
+        LOGGER.info("update question , the ip = [{}]", IpUtil.getIpAddr(httpServletRequest));
         return JsonResult.succResult(questionResult);
 
     }
@@ -70,9 +82,10 @@ public class QuestionController {
      * @param id
      * @return
      */
-    @RequestMapping(value = "/testInfo",method = RequestMethod.GET)
+    @RequestMapping(value = "/testInfo", method = RequestMethod.GET)
     public JsonResult getTestCaseByQuestionId(@RequestParam Integer id) {
         List<TestCase> testCaseByQuestionId = testInfoService.getTestCaseByQuestionId(id);
+        LOGGER.info("get testCaseBy questionId, the ip = [{}]", IpUtil.getIpAddr(httpServletRequest));
         return JsonResult.succResult(testCaseByQuestionId);
 
     }
