146049213ccdb33ada7a40f07763970398cc2274
Yangchen <2015011446@mail.bistu.edu.cn>
2019-05-14 04:40:53 +0800

    记录提交的代码信息到数据库

---
+++ src/main/java/yangchen/exam/entity/Submit.java
@@ -0,0 +1,41 @@
+package yangchen.exam.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * @author YC
+ * @date 2019/5/14 4:30
+ * O(∩_∩)O)
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "submit")
+public class Submit {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "submit_time")
+    private Timestamp submitTime;
+
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "question_id")
+    private Integer questionId;
+
+    @Column(name = "examination_id")
+    private Integer examinationId;
+
+    @Column(name = "src")
+    private String src;
+}
