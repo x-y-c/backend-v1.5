befc505d7bb11687a545d265f736e80150a01bb7
Yangchen <2015011446@mail.bistu.edu.cn>
2019-06-01 16:53:05 +0800

    添加score 数据库层

---
+++ src/main/java/yangchen/exam/entity/Score.java
@@ -0,0 +1,38 @@
+package yangchen.exam.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * @author YC
+ * @date 2019/6/1 15:25
+ * O(∩_∩)O)
+ */
+@Entity
+@Table(name = "score")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Score {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "examination_id")
+    private Integer examinationId;
+
+    @Column(name = "submit_time")
+    private Timestamp submitTime;
+
+    @Column(name = "score")
+    private Integer score;
+
+}
---
+++ src/main/java/yangchen/exam/repo/ScoreRepo.java
@@ -0,0 +1,23 @@
+package yangchen.exam.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import yangchen.exam.entity.Score;
+
+import java.util.List;
+
+/**
+ * @author YC
+ * @date 2019/6/1 15:53
+ * O(∩_∩)O)
+ */
+public interface ScoreRepo extends JpaRepository<Score, Integer> {
+    /**
+     * 通过学号和考试标号查成绩；
+     *
+     * @param studentId
+     * @param examinataionId
+     * @return
+     */
+    List<Score> findByStudentIdAndExaminationId(Long studentId, Integer examinataionId);
+    
+}
