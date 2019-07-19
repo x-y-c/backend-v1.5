adda8ac03ec250b14b76234d27274c92d558cf89
yangchen <13681132624@163.com>
2019-07-20 00:45:19 +0800

    删除空的repo

--- src/main/java/yangchen/exam/repo/StudentNewRepo.java
+++
@@ -1,7 +0,0 @@
-package yangchen.exam.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import yangchen.exam.entity.StudentNew;
-
-public interface StudentNewRepo extends JpaRepository<StudentNew,Integer> {
-}
