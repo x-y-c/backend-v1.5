587ac72cfcc3e2d1e435d31e0447d7bbea01790f
Baby_Yang <327485245@qq.com>
2019-11-10 00:10:08 +0800

    --解决url包含特殊字符报错400的问题--

---
+++ src/main/java/yangchen/exam/Component/PortalTomcatWebServerCustomizer.java
@@ -0,0 +1,15 @@
+package yangchen.exam.Component;
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortalTomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setAttribute("relaxedQueryChars", "{}[]|"));
+    }
+}
