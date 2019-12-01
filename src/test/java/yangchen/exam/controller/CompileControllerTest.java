6e4caf0461cb6f410b047d833e11d81c8a201fa2
Baby_Yang <327485245@qq.com>
2019-12-01 21:37:47 +0800

    配置文件提交

---
+++ src/test/java/yangchen/exam/controller/CompileControllerTest.java
@@ -0,0 +1,19 @@
+//package yangchen.exam.controller;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import static org.junit.Assert.*;
+//
+//@SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
+//public class CompileControllerTest {
+//
+//
+//    @Test
+//    public void test(){
+//        System.out.println(new CompileController().cccheck().toString());
+//    }
+//}
