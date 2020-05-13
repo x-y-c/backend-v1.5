6f97e80d16d6159d7178dbdcbfe4e16e023c1471
jintong <1299894309@qq.com>
2020-05-13 14:51:39 +0800

    新增了"视频教学模块"、"算法教学模块"、"个人练习统计模块"、"题库信息统计模块"及"工程认证模块"。

--- src/main/java/yangchen/exam/repo/QuestionTableRepo.java
+++ src/main/java/yangchen/exam/repo/QuestionTableRepo.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface QuestionTableRepo extends JpaRepository<StudentNew,Integer> {
 
-    @Query(value = "select submit.student_id,student_name,Score,QuestionBh from Questions join student_new join submit where submit.student_id = student_new.student_id and student_new.student_id like CONCAT(?1,'%') and Questions.QuestionBh = submit.question_id and Questions.difficulty=?2 and Questions.stage=?3",nativeQuery = true)
+    @Query(value = "select score.student_id,student_name,score,question_id from score join student_new join question_new where score.student_id = student_new.student_id and student_new.student_id like CONCAT(?1,'%') and question_new.questionBh = score.question_id and question_new.difficulty=?2 and question_new.stage=?3",nativeQuery = true)
     List<Object> showSearch(String studentId, String stage, String diff);
 }
